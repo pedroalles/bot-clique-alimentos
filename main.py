61904e15ac413630f576e5f0fbc0ed04fd792811
pedroalles <pedroalles@gmail.com>
2021-06-28 01:29:21 -0300

    Add headless

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def doar():
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         # chrome_options.binary_location = '/usr/bin/google-chrome'
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
@@ -69,7 +69,7 @@ def doar():
 
         print("Houve algum problema. Repetindo processo.")
         print(e)
-        # driver.quit()
+        driver.quit()
         doar()
 
 schedule.every(3).seconds.do(doar)
